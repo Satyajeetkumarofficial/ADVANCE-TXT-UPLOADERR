cafa9a584210368e12542292c8b59324c1239f79
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2026-01-28 05:54:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -669,26 +669,25 @@ else:
 
 
                 if "drive" in url:
-    try:
-        ka = await helper.download(url, name)
-        copy = await bot.send_document(
-            chat_id=m.chat.id,
-            document=ka,
-            caption=cc1
-        )
-        count += 1
-        os.remove(ka)
-        time.sleep(1)
-
-    except FloodWait as e:
-        await m.reply_text(str(e))
-        time.sleep(e.x)
-        continue
-
-
-elif ".pdf" in url:
-    try:
-        await asyncio.sleep(4)
+                    try:
+                        ka = await helper.download(url, name)
+                        copy = await bot.send_document(
+                            chat_id=m.chat.id,
+                            document=ka,
+                            caption=cc1
+                        )
+                        count += 1
+                        os.remove(ka)
+                        time.sleep(1)
+                    
+                    except FloodWait as e:
+                        await m.reply_text(str(e))
+                        time.sleep(e.x)
+                        continue
+                
+                elif ".pdf" in url:
+                    try:
+                        await asyncio.sleep(4)
         # Replace spaces with %20 in the URL
                         url = url.replace(" ", "%20")
  
